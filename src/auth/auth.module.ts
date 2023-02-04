5d8b678f2948ca420004cebcbe71f965aeacf453
kylie <wlswn4630@gmail.com>
2023-02-04 20:29:41 +0900

    feat: create jwt token

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    forwardRef(() => UsersModule),
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_TOKEN'),
+        signOptions: { expiresIn: '1 day' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
