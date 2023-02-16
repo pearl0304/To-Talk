ca49058210c045fcd3c541986552dcef21003104
kylie <wlswn4630@gmail.com>
2023-02-16 22:12:45 +0900

    feat: Add UserModule into ArticleModule

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { MulterModule } from '@nestjs/platform-express';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     MulterModule.register({
       dest: './upload',
     }),
